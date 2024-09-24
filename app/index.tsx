81ca0803df2df0587e986ea19de5587fe8b4b754
Clemente Streeter <clemente.streeter@uc.cl>
2024-09-24 18:29:55 -0300

    init

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 
 export default function Page() {
+	console.log("e")
 	return (
 		<View style={{ flex: 1 }}>
 			<MapView style={StyleSheet.absoluteFill} />
