87df1fdcab807b1476b8f2e2b5c0bbaa3c443dd7
mguzman20 <m.gguzman@uc.cl>
2024-09-24 22:37:32 -0300

    Refactorizar AuthContext para mejorar la gestión del estado de autenticación

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,99 +1,43 @@
 import { createContext, useContext, useEffect, useState} from 'react';
-import axios from 'axios';
-import * as SecureStore from 'expo-secure-store';
 
 interface AuthProps {
-    authState?: {token: string | null; authenticated: boolean | null };
-    onRegister?: (email: string, password: string) => Promise<any>
-    onLogin?: (email: string, password: string) => Promise<any>;
-    onLogout?: () => Promise<any>;
+    authState: {token: string | null; authenticated: boolean };
+    setAuthState: React.Dispatch<React.SetStateAction<{
+        token: string | null;
+        authenticated: boolean;
+      }>>;
 }
 
-const TOKEN_KEY = 'my-jwt';
-export const API_URL = 'https://api.developbetterapps.com';
-const AuthContext = createContext<AuthProps>({});
+const AuthContext = createContext<AuthProps>({
+    authState: {
+        token: null,
+        authenticated: false
+    },
+    setAuthState: () => {}
+});
 
 
 export const useAuth = () => {
     return useContext(AuthContext)
 };
 
-export const AuthProvider = ({children: any}) => {
+import { ReactNode } from 'react';
+
+
+export const AuthProvider = ({children}: {children: ReactNode}) => {
     const [authState, setAuthState] = useState<{
         token: string | null;
-        authenticated: boolean | null;
+        authenticated: boolean;
     }>({
         token: null,
-        authenticated: null
+        authenticated: false
     });
 
-    useEffect(() => {
-        const loadtoken = async () => {
-            const token = await SecureStore.getItemAsync(TOKEN_KEY);
-            console.log('stored:', token);
-
-            if (token){
-                axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-                setAuthState({
-                    token: token,
-                    authenticated: true,
-                });
-            }
-        };
-        loadtoken();
-    }, []);
-
-    const register = async (email: string, password: string) => {
-        try {
-            return await axios.post(`${API_URL}/users`, {email, password});
-        } catch(e) {
-            return {error: true, msg: (e as any).response.data.msg };
-        }
-    };
-    
-    const login = async (email: string, password: string) => {
-        try {
-            const result = await axios.post(`${API_URL}/auth`, {email, password});
-            
-            console.log(result)
-
-            setAuthState({
-                token: result.data.token,
-                authenticated: true
-            });
-
-            axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.token}`;
-
-            await SecureStore.setItemAsync(TOKEN_KEY, result.data.token);
-
-            return result;
-        } catch(e) {
-            return {error: true, msg: (e as any).response.data.msg };
-        }
-    };
-
-    const logout = async() => {
-        await SecureStore.deleteItemAsync(TOKEN_KEY);
-
-        axios.defaults.headers.common['Authorization'] = '';
-
-        setAuthState({
-            token: null,
-            authenticated: false
-        });
-    };
-
-
-
-    const value = {
-        onRegister: register,
-        onLogin: login,
-        onLogout: logout,
-        authState
-    }
-
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+    return (
+        <AuthContext.Provider value={ { authState, setAuthState }}>
+          {children}
+        </AuthContext.Provider>
+      );
 }
 
 export default AuthProvider;
