16e4f3bd53ee41aa538d2f851fdbbda0f50fb215
mguzman20 <m.gguzman@uc.cl>
2024-09-24 22:37:37 -0300

    Refactorizar AuthContext para mejorar la gestión del estado de autenticación

--- app/Login.tsx
+++ app/Login.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import LoginForm from './Form'
 
 const Login = () => {
     return (
         <View>
-            <Text>Login</Text>
+            <LoginForm/>
         </View>
     );
 };
--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,8 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './Home'
-import Login from './Login'
-import AuthProvider from './context/AuthContext'
+import { Redirect } from 'expo-router';
 
-const Stack = createNativeStackNavigator();
 
 export default function Page() {
-	return (
-		<AuthProvider>
-			<Layout></Layout>
-		</AuthProvider>
-	);
+	
+	return <Redirect href={"/(tabs)/Home"} />;
 }
-
-export const Layout = () => {
-	const { authState, onLogout} = useAuth();
-	return (
-	<NavigationContainer>
-		<Stack.Navigator>
-		{ authState?.authenticated ? (
-			<Stack.Screen 
-			name="Home" 
-			component={Home}
-			options={{
-				headerRight: () => <Button onPress={onLogout} title="SignOut"/>,
-			}}></Stack.Screen>
-		):(
-			<Stack.Screen name='Login' component={Login}></Stack.Screen>
-		)}
-		</Stack.Navigator>
-	</NavigationContainer>
-	);
-};
