f85865fa862a9d8a9a62f8cce6c6618d737a7361
kovaxis <martin.andrighetti@gmail.com>
2024-11-27 14:31:05 -0300

    Mostrar fechas de eventos

--- app/helpers/util.ts
+++ app/helpers/util.ts
@@ -3,3 +3,10 @@ export function capitalize(s: string): string {
     if (s == null) return s;
     return s.substring(0, 1).toUpperCase() + s.substring(1)
 }
+
+
+export function formatDate(date?: Date): string {
+    if (!date) return "-"
+    const d = new Date(date)
+    return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`
+}
